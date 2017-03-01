c08f16257d34b9cc94c4c133b5795d4564d1aaf4
Gabe Zimbric <g.zimbric@gmail.com>
2017-03-01 07:27:56 -0600

    final

--- fblamobileapp/CommentViewController.swift
+++ fblamobileapp/CommentViewController.swift
@@ -16,7 +16,7 @@ class CommentViewController: UIViewController {
     
     var commentDetails: String?
     let commentID = UUID().uuidString
-    let timestamp = FIRServerValue.timestamp()
+    let timestamp = Date().iso8601
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,5 +68,25 @@ class CommentViewController: UIViewController {
                 }
             })
         }
+    }
+}
+
+extension Date {
+    static let iso8601Formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+        return formatter
+    }()
+    var iso8601: String {
+        return Date.iso8601Formatter.string(from: self)
+    }
+}
+
+extension String {
+    var dateFromISO8601: Date? {
+        return Date.iso8601Formatter.date(from: self)
     }
 }
